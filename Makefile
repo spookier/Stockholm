195355732cebfeaa948e23637a6cdb1c8e145cbc
Alexandru Costin <spxxky.dev@gmail.com>
2026-01-14 12:53:23 +0100

    Restructure

--- Makefile
+++ Makefile
@@ -1,25 +1,30 @@
 NAME = stockholm
 
 CC = cc
-CFLAGS = -Wall -Wextra -Werror
+CFLAGS = -Wall -Wextra -Werror -I$(INC_DIR)
 LIBS = -lsodium
 
-SRC = main.c \
-	  Encrypt.c \
-	  Decrypt.c
+SRC_DIR = srcs
+INC_DIR = incs
+OBJ_DIR = objs
 
-OBJ = $(SRC:.c=.o)
+SRC = $(SRC_DIR)/main.c \
+	  $(SRC_DIR)/Encrypt.c \
+	  $(SRC_DIR)/Decrypt.c
+
+OBJ = $(SRC:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
 
 all: $(NAME)
 
 $(NAME): $(OBJ)
 		$(CC) $(OBJ) $(LIBS) -o $(NAME)
 
-%.o: %.c
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+		@mkdir -p $(OBJ_DIR)
 		$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
-		rm -f $(OBJ)
+		rm -rf $(OBJ_DIR)
 
 fclean: clean
 		rm -f $(NAME)
---
+++ srcs/Encrypt.c
@@ -0,0 +1,45 @@
+#include "../incs/Encrypt.h"
+
+int encrypt(const char *filepath, unsigned char *hashed_key, int silent) {
+  (void)filepath;
+  (void)silent;
+
+  FILE *file;
+  long size;
+  unsigned char *plaintext;
+
+  file = fopen("test_file.txt", "rb");
+
+  // get size
+  fseek(file, 0, SEEK_END);
+  size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  // read and store in memory
+  plaintext = malloc(size);
+  fread(plaintext, 1, size, file);
+  fclose(file);
+
+  // nonce
+  unsigned char nonce[crypto_secretbox_NONCEBYTES];
+  randombytes_buf(nonce, sizeof(nonce));
+
+  // signature to check if data has been tempered with
+  unsigned char *signature = malloc(crypto_secretbox_MACBYTES + size);
+
+  // encrypt
+  crypto_secretbox_easy(signature, plaintext, size, nonce, hashed_key);
+
+  // 4. Write to file.ft: nonce FIRST, then ciphertext
+  FILE *out = fopen("test_file.txt", "wb");
+  fwrite(nonce, 1, sizeof(nonce), out);
+  fwrite(signature, 1, crypto_secretbox_MACBYTES + size, out);
+  fclose(out);
+
+  // 5. Clean up
+  free(signature);
+  free(plaintext);
+
+  printf("Encrypted!\n");
+  return (0);
+}
